5dc98a5b1943b861ce73fcb82a88bdbbbaa67190
Acetab <1160517417@qq.com>
2026-08-25 18:54:33 +0800

    build: 刷新 dist/ 词库引用至 f993d22（Get disappointed 修复）

--- dist/steamdb-zh-cn.user.js
+++ dist/steamdb-zh-cn.user.js
@@ -7,7 +7,7 @@
 // @match         https://steamdb.info/*
 // @run-at        document-idle
 // @grant         GM_getResourceText
-// @resource      dictTranslations https://cdn.jsdelivr.net/gh/Acetab/steamdb-zh-cn@e9775353f87159d07c8c774906fed9acb061f1a3/translations.zh-CN.json
+// @resource      dictTranslations https://cdn.jsdelivr.net/gh/Acetab/steamdb-zh-cn@f993d22262602514b5b2ce3891a039999ff5f4e2/translations.zh-CN.json
 // @connect       cdn.jsdelivr.net
 // @license       MIT
 // ==/UserScript==
